6b849993cd41f8c5b77a6e943463758821197ea1
Una <una_chou@sina.com>
2018-08-05 15:53:20 +0800

    city header

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,16 +1,18 @@
 <template>
     <div class="header">
         <div class="header-left">
-            <div class="iconfont back-icon">&#xe624;</div>
+          <div class="iconfont back-icon">&#xe624;</div>
         </div>
         <div class="header-input">
-            <span class="iconfont">&#xe632;</span>
-            输入城市/景点/游玩主题
+          <span class="iconfont">&#xe632;</span>
+          输入城市/景点/游玩主题
         </div>
-        <div class="header-right">
+        <router-link to="/city">
+          <div class="header-right">
             {{this.city}}
             <span class="iconfont arrow-icon">&#xe64a;</span>
-        </div>
+          </div>
+        </router-link>
     </div>
 </template>
 
